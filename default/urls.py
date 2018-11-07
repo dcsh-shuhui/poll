e8d61c1aa277b41a50de8bd0e6f03e634cf85b0f
Dino Wei <idreamer@gmail.com>
2018-11-07 15:52:50 +0800

    投票主題列表

---
+++ default/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('poll/', views.PollList.as_view()),
+]
--- default/views.py
+++ default/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
+from django.views.generic import ListView
+from .models import *
 
-# Create your views here.
+## Create your views here.
+## 投票主題列表
+class PollList(ListView):
+    model = Poll
